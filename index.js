018ea4976c825839f8086b9af2aa731ef6c9e001
Anthony Daniel <39199700+AnthonyDaniel@users.noreply.github.com>
2020-09-27 15:41:28 -0600

    Templatejs

---
+++ index.js
@@ -0,0 +1,25 @@
+var Blog = {
+    url: "1",
+    title: "2",
+    description: "3",
+    content1: "4",
+    url2: "5",
+    content2: "6",
+    url3: "7",
+    urlvideo: "8"
+}
+
+loadPreBlog();
+
+function loadPreBlog() {
+
+    var title = null;
+
+    title = document.getElementById("title");
+
+    console.log(Blog);
+}
+
+function loadBlog() {
+
+}
